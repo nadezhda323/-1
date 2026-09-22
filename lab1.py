4aaf167cd9a6ccb79b1ef4266aad31d304abd432
Свят Кныш <svyat.knysh@gmail.com>
2026-09-22 12:20:40 +0300

    created new file lab1.py

---
+++ lab1.py
@@ -0,0 +1,18 @@
+nameoper = input()
+if nameoper == "+":
+    cifra = int(input())
+    cifradva = int(input())
+    itog = cifra + cifradva
+if nameoper == "*":
+    cifra = int(input())
+    cifradva = int(input())
+    itog = cifra * cifradva
+if nameoper == "-":
+    cifra = int(input())
+    cifradva = int(input())
+    itog = cifra - cifradva
+if nameoper == "/":
+    cifra = int(input())
+    cifradva = int(input())
+    itog = cifra / cifradva
+print(itog)
